c6cbc697da6ddf6f5a602aa02565746ffc66c586
Daniel Gaytan <dgaytan6@gmail.com>
2016-10-27 14:30:23 -0500

    Log history

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   end
 
   def history
+    @games = current_user.games.includes(:players_games)
   end
 
   def log
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :players_games, foreign_key: :player_id
+  has_many :games, through: :players_games
+
   devise :database_authenticatable, :registerable, :trackable, :validatable
 end
