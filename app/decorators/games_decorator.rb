29bc4692336e29e5a1293babb86ecd5442c5cc5a
Daniel Gaytan <dgaytan6@gmail.com>
2016-10-27 14:58:38 -0500

    Improve games decorator

--- app/decorators/games_decorator.rb
+++ app/decorators/games_decorator.rb
@@ -6,33 +6,33 @@ class GamesDecorator
   end
 
   def games
-    user.games.includes(:players_games, :players).map do |game|
+    user.games.includes(:players_games, :players).order('games.played_at DESC').map do |game|
       GameDecorator.new(game, me: user)
     end
   end
-end
 
-class GameDecorator
-  attr_reader :game, :me
+  class GameDecorator
+    attr_reader :game, :me
 
-  def initialize(game, me:)
-    @game = game
-    @me = me
-  end
+    def initialize(game, me:)
+      @game = game
+      @me = me
+    end
 
-  def played_at
-    game.played_at
-  end
+    def played_at
+      game.played_at.strftime('%b %d, %Y')
+    end
 
-  def opponent
-    game.players.first{|player| player != me}.email
-  end
+    def opponent
+      game.players.first{|player| player != me}.email
+    end
 
-  def score
-    game.players_games.map(&:score).join(' - ')
-  end
+    def score
+      game.players_games.map(&:score).join(' - ')
+    end
 
-  def result
-    game.winner_id == me.id ? 'W' : 'L'
+    def result
+      game.winner_id == me.id ? 'W' : 'L'
+    end
   end
 end
